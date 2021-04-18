75a52714834a5dc54ee6e6373545b4cafad229fd
luanarmo <ang3l_morales800@live.com>
2021-04-17 22:01:19 -0500

    env variables added

--- config/db.php
+++ config/db.php
@@ -1,10 +1,18 @@
 <?php
 
+//require_once('vendor/autoload.php');
+\Dotenv\Dotenv::createImmutable(__DIR__.'/../')->load();
+
+$host = $_ENV['host'];
+$db_name = $_ENV['db_name'];
+$username = $_ENV['username'];
+$password = $_ENV['password'];
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2_crash_course',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => "mysql:host=$host;dbname=$db_name",
+    'username' => "$username",
+    'password' => "$password",
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
